6947d91fd9841fca5bb1910fac3ad235c208d930
Hypotex <thomas.roger@epitech.eu>
2026-05-07 10:54:56 +0200

    feat: add readline.h for interactive line editor API

---
+++ include/readline.h
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2026
+** readline.h
+** File description:
+** interactive line editor API
+*/
+
+#ifndef READLINE_H_
+    #define READLINE_H_
+
+    #include <termios.h>
+    #include <unistd.h>
+    #include "shell.h"
+
+    #define ARROW_UP '\033'
+
+typedef struct {
+    char *buf;
+    size_t len;
+    char **hist;
+    int hist_count;
+    int hist_idx;
+} rl_ctx_t;
+
+struct termios raw_mode(void);
+void disable_raw_mode(struct termios *orig);
+char **load_history(int *count);
+void free_history(char **entries, int count);
+char *read_line(shell_t *shell);
+
+#endif // !READLINE_H_
